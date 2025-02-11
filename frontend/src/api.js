5069614991f25b85a5acd873dc38d674de092433
VarnankSharma <varnanksharma1@gmail.com>
2025-02-11 18:28:08 +0530

    Fixed backend structure for Vercel

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://blog-site-sc6u.vercel.app/";
+const API_URL = "https://blog-site-sc6u.vercel.app/api/post";
 
 export const createPost = async (postData) => {
   return await axios.post(API_URL, postData);
